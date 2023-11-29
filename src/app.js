8a85707d357e3169f2e39476336b001972a9ca8c
Ivy070109 <Ivyaguilar07@gmail.com>
2023-11-29 19:59:05 -0300

    creación de chat y funcionalidad ok

--- src/app.js
+++ src/app.js
@@ -1,18 +1,19 @@
 import express from "express"
 import mongoose from "mongoose"
-import productRouter from "./routes/product.router.js"
-import cartRouter from "./routes/carts.router.js"
 import handlebars from "express-handlebars"
-import viewsRouter from './routes/views.router.js'
-//import ProductManager from "./components/ProductManager.js"
 import { Server } from "socket.io"
 import { __dirname } from "./utils.js"
+
+import productRouter from "./routes/product.router.js"
+import cartRouter from "./routes/carts.router.js"
+import viewsRouter from './routes/views.router.js'
 //importo la nueva ubicación de los sockets products
 import socketProducts from './socket/socketProduct.js'
+import socketChat from './socket/socketChat.js'
 
 const app = express()
 const PORT = 8080
-//const productManager = new ProductManager("/files/products.json")
+
 const MONGOOSE_URL = 'mongodb+srv://ivyaguilar07:bjLpjWzJQGdcrVRL@cluster0.2olteyc.mongodb.net/ecommerce'
 
 const httpServer = app.listen(PORT, () => {
@@ -21,9 +22,6 @@ const httpServer = app.listen(PORT, () => {
 
 try {
     await mongoose.connect(MONGOOSE_URL)
-    // app.listen(PORT, () => {
-    //     console.log(`Servidor Express ejecutándose en puerto ${PORT}`)
-    // })
     httpServer
 } catch (err) {
     console.log(`No se puede conectar con las bases de datos (${err.message})`)
@@ -53,5 +51,5 @@ app.use('/', viewsRouter)
 
 //pasaré las conecciones socket a una nueva carpeta 
 socketProducts(socketServer)
-
+socketChat(socketServer)
 
